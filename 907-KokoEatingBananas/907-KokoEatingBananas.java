7d2249a785533e4ad8127fd20e834674b8ada403
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-09-27 21:19:28 +0530

    Create 907-KokoEatingBananas

---
+++ 907-KokoEatingBananas/907-KokoEatingBananas.java
@@ -0,0 +1,26 @@
+// Last updated: 9/27/2025, 9:19:28 PM
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int start = 1;  
+        int end = 0;
+        for (int pile : piles) {
+            end = Math.max(end, pile); 
+        }
+        
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int totalHours = 0;
+            for (int pile : piles) {
+                totalHours += (pile + mid - 1) / mid;  
+            }
+            
+            if (totalHours > h) {
+                start = mid + 1;  
+            } else {
+                end = mid;  
+            }
+        }
+        
+        return start;  
+    }
+}
